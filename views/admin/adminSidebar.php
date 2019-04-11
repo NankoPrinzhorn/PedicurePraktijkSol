d50778c9998e9318d061036c0f61d49af4109898
mauro <maurobertozzi11@gmail.com>
2019-04-11 10:56:35 +0200

    title

--- views/admin/adminSidebar.php
+++ views/admin/adminSidebar.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="/css/admin/adminSidebar.css">
 <script type="text/javascript" src="/js/admin/adminSidebar.js"></script>
 <div id="sidebar">
-	dsfasdf
+	<h1>CMS</h1>
 	<button class="accordion">Section 1</button>
 	<div class="panel">
 	  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
