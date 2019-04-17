b777026b68907b3a21c3b9cf4e51094bfd226073
NankoPrinzhorn <memaybeme@hotmail.com>
2019-04-17 15:05:49 +0200

    logout fixed and changes

---
+++ model/requests/logoutSubmit.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+//set database
+include_once "../../database/Database.php";
+include_once "../../database/SiteDatabase.php";
+$db = new SiteDatabase();
+if (!empty($_GET) && $_GET['value'] == 'logout') {
+    session_destroy();
+    echo json_encode(true); 
+}
+?>
