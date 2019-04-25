2abb80170a00d5a21ae8d245da1f93f530e3144d
Ravi Stil <ravistil@MacBook-Pro-van-Ravi.local>
2019-04-25 09:48:52 +0200

    Commit

--- views/site/home.php
+++ views/site/home.php
@@ -1,6 +1,7 @@
 <?php 
 include_once $_SERVER['DOCUMENT_ROOT']."/model/site/home.php";
 ?>
+<link rel="stylesheet" type="text/css" href="/css/site/menu.css">
 <link rel="stylesheet" type="text/css" href="/css/site/home.css">
 <body>
   <nav class="navbar">
@@ -26,7 +27,18 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/site/home.php";
       </li>
     </ul>
   </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="bootstrap-test col-md-6">col-md-6</div>
+      <div class="bootstrap-test col-md-4">col-md-4</div>
+      <div class="bootstrap-test col-md-1">col-md-1</div>
+      <div class="bootstrap-test col-md-1">col-md-1</div>
+    </div>
+  </div>
+
 </body>
+
 <script type="text/javascript" src="/js/site/home.js"></script>
 
 <p style="white-space: pre-wrap;" class="<?=$data[1]['id']."-".str_replace(" ", "_", $data[1]['htmlID'])?>"><?=$data[1]['content']?></p>
