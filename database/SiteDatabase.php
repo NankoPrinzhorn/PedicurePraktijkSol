4c69c604039bb6c7381be132a9104a02f4d01ba0
NankoPrinzhorn <memaybeme@hotmail.com>
2019-05-07 17:27:43 +0200

    koppel tabel added!!!

--- database/SiteDatabase.php
+++ database/SiteDatabase.php
@@ -67,40 +67,137 @@ class SiteDatabase extends Database {
     public function generateCMSHTML($data) {
         // var_dump($data);
         foreach ($data as $item) {
+            $item['koppel_tabel'] = 0;
+            $item['Field'] = 0;
             //text -> textfield
             //varchar -> text input
             //boolean -> ja nee vraag
+            if ($item["inputType"] == "text" || 
+                $item["inputType"] == "varchar(255)") {
+                $this->showCMSHTML($item);
+            } elseif (strpos($item["inputType"], 'koppel_') !== false) {    
+                $koppel_tabel = substr($item["inputType"], 7);
+                $koppel_columns = $this->fetchAll("SHOW COLUMNS FROM `$koppel_tabel`");
+                $koppel_info = $this->fetchAll("SELECT * FROM `$koppel_tabel` WHERE deleted = 0 ORDER BY pageOrder ASC");
+                $count = 0;
+                foreach ($koppel_info as $item) {
+                    $current_item = [];
+                    echo "<div class='innerCMSBlok'>";
+                    $count++;
+                    echo "<h1>".substr(str_replace("concept_", "", $koppel_tabel), 0, -2) ." ".$count."</h1>";
+                    foreach($koppel_columns as $column) {
+                        if ($column["Type"] == "text" || 
+                            $column["Type"] == "varchar(255)" ||
+                            $column["Type"] == "int(1)") {
 
-            $currentType = "";
-            switch ($item["inputType"]) {
-                case "text":
-                    echo "<label for=".$item["editID"]." >".$item["htmlID"]."</label><textarea id='".$item["editID"]."' style='width: 100%;'>".$item["text"]."</textarea>";
-                    break;
-                case "varchar":
-                    echo "<label for=".$item["editID"]." >".$item["htmlID"]."</label><input id='".$item["editID"]."' style='width: 100%;' type='text' value='".$item["text"]."'>";
-                    break;
-            }
-            echo "
-            <script>
-                $('#".$item["editID"]."').on('keyup', function() {
-                    $('.".$item["editID"]."').text($(this).val());
-                    if(ajaxListRequest) {
-                        ajaxListRequest.abort();
-                        ajaxListRequest = null;
+                            $array = [
+                                'id' => $item['id'],
+                                'inputType' => $column["Type"],
+                                'Field' => $column["Field"],
+                                'editID' => $item['id']."-".$koppel_tabel."-".$column['Field'],
+                                'htmlID' => str_replace("concept_", "", $koppel_tabel),
+                                'text' => $item[$column['Field']],
+                                'koppel_tabel' => $koppel_tabel
+                            ];
+                            array_push($current_item, $array);
+                        }
                     }
 
-                    ajaxListRequest = $.ajax({
-                        url:'/model/requests/updateConcept.php',
-                        data: {
-                            method:'POST',
-                            id:".$item["id"].",
-                            value:$(this).val(),
-                        }
-                    });
-                });
-            </script>
-            ";
-            // echo ($item["htmlID"]);
+                    foreach($current_item as $item) {
+                        $this->showCMSHTML($item);
+                    }
+                    echo "</div>";
+                }
+            }
+        }
+    }
+
+    public function showCMSHTML($item) {
+        switch ($item["inputType"]) {
+            case "text":
+                echo "<label for=".$item["editID"].">".$item["htmlID"]."</label><textarea id='".$item["editID"]."' style='width: 100%;'>".$item["text"]."</textarea>";
+                echo "
+                    <script>
+                        $('#".$item["editID"]."').on('keyup', function() {
+                            $('.".$item["editID"]."').text($(this).val());
+                            if(ajaxListRequest) {
+                                ajaxListRequest.abort();
+                                ajaxListRequest = null;
+                            }
+
+                            ajaxListRequest = $.ajax({
+                                url:'/model/requests/updateConcept.php',
+                                data: {
+                                    id:".$item["id"].",
+                                    koppel_tabel:'".$item['koppel_tabel']."',
+                                    koppel_type:'".$item['Field']."',
+                                    value:$(this).val()
+                                }
+                            });
+                        });
+                    </script>
+                ";
+                break;
+
+            case "varchar(255)":
+                echo "<label for=".$item["editID"].">".$item["htmlID"]."</label><input id='".$item["editID"]."' style='width: 100%;' type='text' value='".$item["text"]."'>";
+                echo "
+                    <script>
+                        $('#".$item["editID"]."').on('keyup', function() {
+                            $('.".$item["editID"]."').text($(this).val());
+                            if(ajaxListRequest) {
+                                ajaxListRequest.abort();
+                                ajaxListRequest = null;
+                            }
+
+                            ajaxListRequest = $.ajax({
+                                url:'/model/requests/updateConcept.php',
+                                data: {
+                                    id:".$item["id"].",
+                                    koppel_tabel:'".$item['koppel_tabel']."',
+                                    koppel_type:'".$item['Field']."',
+                                    value:$(this).val()
+                                }
+                            });
+                        });
+                    </script>
+                ";
+                break;
+
+            case "int(1)":
+                if ($item['Field'] == "weergeven") {
+                    $ja = ($item['text'] == 1) ? "checked" : "";
+                    $nee = ($item['text'] == 0) ? "checked" : "";
+
+                    echo "weergeven:";
+                    echo "<input id='".$item["editID"]."' type='radio' name='weergeven-".$item["editID"]."' value='1' $ja>Ja";
+                    echo "<input id='".$item["editID"]."' type='radio' name='weergeven-".$item["editID"]."' value='0' $nee>Nee";
+                
+                    echo "
+                        <script>
+                            $('input[type=radio][name=weergeven-".$item["editID"]."]').on('change', function() {
+                                if(ajaxListRequest) {
+                                    ajaxListRequest.abort();
+                                    ajaxListRequest = null;
+                                }
+
+                                ajaxListRequest = $.ajax({
+                                    url:'/model/requests/updateConcept.php',
+                                    data: {
+                                        id:".$item["id"].",
+                                        koppel_tabel:'".$item['koppel_tabel']."',
+                                        koppel_type:'".$item['Field']."',
+                                        value:$(this).val()
+                                    }
+                                });
+                            });
+                        </script>
+                    ";
+                }
+                break;
+            
+            default:
+                break;
         }
     }
 }
--- model/requests/updateConcept.php
+++ model/requests/updateConcept.php
@@ -4,13 +4,19 @@ include_once "../../database/Database.php";
 include_once "../../database/SiteDatabase.php";
 $db = new SiteDatabase();
 
-// var_dump($_GET);
 if(!empty($_GET)) {
-    $id = $_GET['id'];
-    $value = $_GET['value'];
+    if (!empty($_GET['koppel_tabel'])) {
+        $sql = "UPDATE `".$_GET['koppel_tabel']."` SET `".$_GET['koppel_type']."` = ? WHERE id = ?";
+        $params = array($_GET['value'], $_GET['id']);
 
-    $sql = "UPDATE concept SET content = ? WHERE id = ?";
-    $params = array($value, $id);
-    $db->performQuery($sql, $params);
+        $db->performQuery($sql, $params);
+    } else {
+        $id = $_GET['id'];
+        $value = $_GET['value'];
+
+        $sql = "UPDATE concept SET content = ? WHERE id = ?";
+        $params = array($value, $id);
+        $db->performQuery($sql, $params);
+    }
 }
 ?>
