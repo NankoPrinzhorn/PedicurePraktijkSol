6ab0ccc6663018284e4f317f519c896115e13f51
Ravi Stil <ravistil@MacBook-Pro-van-Ravi.local>
2019-05-07 09:27:38 +0200

    Merge

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ $db = new SiteDatabase();
 		<link rel="stylesheet" type="text/css" href="/css/framework.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display:400,700" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display:300,400,700" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 		<?php 
 		if (file_exists($_SERVER['DOCUMENT_ROOT']."/css".$_SERVER['REQUEST_URI'].".css")) {
@@ -31,6 +31,7 @@ $db = new SiteDatabase();
 		<script src="/js/framework.js"></script>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js"></script>
 		<?php 
 		if (file_exists($_SERVER['DOCUMENT_ROOT']."/js".$_SERVER['REQUEST_URI'].".js")) {
 			echo "<script src='/js".$_SERVER['REQUEST_URI'].".js'></script>";
--- views/site/home.php
+++ views/site/home.php
@@ -3,18 +3,36 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/site/home.php";
 ?>
 <link rel="stylesheet" type="text/css" href="/css/site/home.css">
 
-<div id="header" class="header">
+<section class="home-hero">
+  <div class="container">
+    <div class="row">
+      <div class="intro col-md-12">
+        <div class="col-md-12">
+        <h1>Voor iedereen die verzorgde voeten wil</h1>
+        <p>Medisch pedicure en beoefen mijn vak met hart en ziel in Alkmaar West en 1 dag per week in Castricum. Of het nu om een ontspannende en cosmetisch verzorgende behandeling gaat of om een behandeling die gericht is op het verlichten van pijnlijke ongemakken aan de voeten. Vakkundig in het wegnemen van pijn door ingroeiende nagels, likdoorns of kloven. Nauwkeurig en zorgzaam in de behandeling van alle voeten die zorg vragen.</p>
+        <button>Meer informatie</button>
+        <button>Neem contact op</button>
+        </div>
+      </div>
+  </div>
+</section>
+
+<section class="home-blocks">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h1>Test</h1>
+
+        <div class="col-md-4">
+          <div class="col-md-12 case" style="background: white; padding-top: 20px; padding-bottom: 20px;">
+            <h3>Test</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, dolor!</p>
+          </div>
+        </div>
+
+
+
       </div>
     </div>
   </div>
-</div>
+</section>
 
-
-</body>
-
-
-<p style="white-space: pre-wrap;" class="<?=$data[1]['id']."-".str_replace(" ", "_", $data[1]['htmlID'])?>"><?=$data[1]['content']?></p>
