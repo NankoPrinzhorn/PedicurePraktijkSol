d2e50aebb529af010d6075664378a6c625719327
Maurokaan <19878@ma-web.nl>
2019-05-08 12:47:13 +0200

    Test

--- views/site/behandelingen.php
+++ views/site/behandelingen.php
@@ -30,7 +30,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/site/home.php";
 	</div>
 </section>
 
-<section id="behandelingen">
+<section id="behandelingen--section">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
