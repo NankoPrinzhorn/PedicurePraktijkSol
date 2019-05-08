013ce68a1ba51b9ef45dfa229b2731c4b69a04ba
NankoPrinzhorn <memaybeme@hotmail.com>
2019-05-08 14:29:17 +0200

    image uploader half done

--- model/functions.php
+++ model/functions.php
@@ -1,8 +1,4 @@
 <?php 
-function createCMSHTML($item) {
-    var_dump($item);
-}
-
 function checkLogin() {
     if (!isset($_SESSION['user_id'])) {
         header("Location: /admin/login");
--- model/requests/updateConcept.php
+++ model/requests/updateConcept.php
@@ -3,12 +3,16 @@
 include_once "../../database/Database.php";
 include_once "../../database/SiteDatabase.php";
 $db = new SiteDatabase();
-
 if(!empty($_GET)) {
     if (!empty($_GET['koppel_tabel'])) {
-        $sql = "UPDATE `".$_GET['koppel_tabel']."` SET `".$_GET['koppel_type']."` = ? WHERE id = ?";
-        $params = array($_GET['value'], $_GET['id']);
-
+        if(empty($_GET['type'])) {
+            $sql = "UPDATE `".$_GET['koppel_tabel']."` SET `".$_GET['koppel_type']."` = ? WHERE id = ?";
+            $params = array($_GET['value'], $_GET['id']);    
+        } else {
+            if ($_GET['type'] == "image") {
+                var_dump($_GET);
+            }
+        }
         $db->performQuery($sql, $params);
     } else {
         $id = $_GET['id'];
@@ -17,6 +21,38 @@ if(!empty($_GET)) {
         $sql = "UPDATE concept SET content = ? WHERE id = ?";
         $params = array($value, $id);
         $db->performQuery($sql, $params);
+    }
+}
+
+if(!empty($_FILES)) {
+    /* Getting file name */
+    $filename = $_POST['id']."-".$_FILES['image']['name'];
+
+    /* Location */
+    $location = $_SERVER['DOCUMENT_ROOT']."/images/".$filename;
+    $htmlLocation = $filename;
+    $uploadOk = 1;
+    $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
+
+    /* Valid Extensions */
+    $valid_extensions = array("jpg","jpeg","png");
+    /* Check file extension */
+    if(!in_array(strtolower($imageFileType),$valid_extensions) ) {
+        $uploadOk = 0;
+    }
+
+    if($uploadOk == 0){
+        echo 0;
+    }else{
+        /* Upload file */
+        if(move_uploaded_file($_FILES['image']['tmp_name'],$location)){
+            if(!empty($_POST['koppel_tabel'])) {
+                $db->performQuery("UPDATE `".$_POST['koppel_tabel']."` SET image = '$htmlLocation' WHERE id = ".$_POST['id']);
+            }
+            echo $htmlLocation;
+        }else{
+            echo 0;
+        }
     }
 }
 ?>
