831bfa21469a76199e570d710d27c122c4d60fca
NankoPrinzhorn <memaybeme@hotmail.com>
2019-05-13 11:29:23 +0200

    Update pedicurepraktijksol.sql

--- pedicurepraktijksol.sql
+++ pedicurepraktijksol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 13, 2019 at 07:12 AM
+-- Generation Time: May 13, 2019 at 09:29 AM
 -- Server version: 5.7.24-log
 -- PHP Version: 7.2.10
 
@@ -46,12 +46,15 @@ INSERT INTO `concept` (`id`, `page`, `pageOrder`, `content`, `inputType`, `htmlI
 (1, 'home', 2, 'Medisch pedicure en beoefen mijn vak met hart en ziel in Alkmaar West en 1 dag per week in Castricum. Of het nu om een ontspannende en cosmetisch verzorgende behandeling gaat of om een behandeling die gericht is op het verlichten van pijnlijke ongemakken aan de voeten. Vakkundig in het wegnemen van pijn door ingroeiende nagels, likdoorns of kloven. Nauwkeurig en zorgzaam in de behandeling van alle voeten die zorg vragen', 'text', 'Tekst onder title', '2019-05-09 11:46:32'),
 (2, 'home', 1, 'Voor iedereen die verzorgde voeten wilt', 'varchar(255)', 'Titel', '2019-05-09 13:12:07'),
 (5, 'home', 3, '', 'koppel_concept_behandelingen', 'Behandelingen', '2019-05-07 12:24:43'),
-(6, 'over-mij', 2, 'Mijn naam is Joke Sol, ik ben gediplomeerd medisch pedicure', 'text', 'Tekst onder titel', '2019-05-09 11:48:15'),
+(6, 'over-mij', 2, 'Mijn naam is Joke Sol, ik ben gediplomeerd medisch pedicure', 'text', 'Tekst onder titel', '2019-05-13 08:50:50'),
 (8, 'over-mij', 4, 'Na een carriere als loopbaanadviseur ontstond bij mijzelf de behoefte aan verandering.\n\n\nEen praktisch beroep waarbij ik mensen blij kon maken. Zo kwam het pedicurewerk voor mij in beeld: mensen helpen door de druk of pijn die ze aan hun voeten hebben voor een groot deel weg te nemen.\n\n\nTijdens mijn opleiding merkte ik hoe fijn het is om mensen weer op weg te helpen naar gezonde, verzorgde en minder pijnlijke voeten. De keuze om na de branche-opleiding pedicure door te leren voor Medisch Pedicure was dan ook vanzelfsprekend voor mij. Ik hoop nog heel lang mensen te kunnen helpen met goede voetzorg en nieuwe ontwikkelingen in het vakgebied te kunnen volgen en gebruiken voor mijn klanten.', 'text', 'Tekst van het blok', '2019-05-09 11:50:12'),
 (9, 'over-mij', 3, 'Loopbaanswitch', 'varchar(255)', 'Title van het blok', '2019-05-09 11:50:05'),
-(7, 'over-mij', 1, 'Uw voeten, in goede handen, daar maak ik graag werk van', 'varchar(255)', 'Titel', '2019-05-13 06:50:42'),
+(7, 'over-mij', 1, 'Uw voeten, in goede handen, daar maak ik graag werk van', 'varchar(255)', 'Titel', '2019-05-13 08:50:42'),
 (10, 'over-mij', 4, '10-HD-Wallpapers-C76.jpg', 'longtext', 'Image van het blok', '2019-05-09 13:09:18'),
-(11, 'home', 3, 'Kom op schoenen die u al enige tijd heeft/draagt, ze geven informatie over drukpunten en hoe u de voeten belast.', 'varchar(255)', 'Tekst onderaan', '2019-05-09 13:11:25');
+(11, 'home', 3, 'Kom op schoenen die u al enige tijd heeft/draagt, ze geven informatie over drukpunten en hoe u de voeten belast.', 'varchar(255)', 'Tekst onderaan', '2019-05-09 13:11:25'),
+(12, 'behandelingen', 1, 'Uw stap op weg naar verzorgde voeten', 'varchar(255)', 'Title', '2019-05-13 09:17:02'),
+(13, 'behandelingen', 2, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus hic sapiente sed unde repudiandae quaerat eligendi, facilis dolorum molestiae obcaecati.', 'text', 'Tekst onder titel', '2019-05-13 09:17:04'),
+(14, 'behandelingen', 3, 'Als er sprake is van klachten aan de voeten bijvoorbeeld door diabetes, reuma, bewegingsbeperking, of andere redenen dan wordt voorafgaand aan de behandeling een intake en voetonderzoek bij u afgenomen Zo wordt geïnventariseerd wat verlichting van de klachten zou kunnen brengen. Een behandelplan wordt opgesteld, thuisadvies wordt meegegeven en vervolgafspraken worden gemaakt', 'text', 'Tekst in blok', '2019-05-13 09:17:31');
 
 -- --------------------------------------------------------
 
@@ -75,7 +78,7 @@ CREATE TABLE `concept_behandelingen` (
 --
 
 INSERT INTO `concept_behandelingen` (`id`, `weergeven`, `deleted`, `title`, `text`, `image`, `pageOrder`, `updated_at`) VALUES
-(1, 1, 0, 'Behandeling met intake', 'Doel van het voetonderzoek is te inventariseren wat uw voetklachten zijn en waar deze uit voort kunnen komen/mee samenhangen; of er sprake is van een risicovoet; met welke behandeling uw klachten verlicht kunnen worden.', '1-40475070_1905388643090316_8129944058334428779_n.jpg', 1, '2019-05-13 06:43:08'),
+(1, 1, 0, 'Behandeling met intake', 'Doel van het voetonderzoek is te inventariseren wat uw voetklachten zijn en waar deze uit voort kunnen komen/mee samenhangen; of er sprake is van een risicovoet; met welke behandeling uw klachten verlicht kunnen worden.', '1-thumb-1920-411820.jpg', 1, '2019-05-13 08:23:58'),
 (2, 0, 1, 'test tekst', 'ietjse meer test ', 'behandeling.png', 2, '2019-05-09 08:36:03'),
 (18, 0, 1, NULL, NULL, NULL, 3, '2019-05-08 10:08:05'),
 (19, 0, 1, NULL, NULL, NULL, 4, '2019-05-08 10:08:01'),
@@ -133,7 +136,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `concept`
 --
 ALTER TABLE `concept`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `concept_behandelingen`
