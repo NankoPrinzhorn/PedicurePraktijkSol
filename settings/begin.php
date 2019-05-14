cc97f67662572b453f81a0937e08ec5e394d281f
NankoPrinzhorn <memaybeme@hotmail.com>
2019-05-14 09:36:35 +0200

    Update begin.php

--- settings/begin.php
+++ settings/begin.php
@@ -1,15 +1,19 @@
 <?php 
+$whitelist = array(
+    '127.0.0.1',
+    '::1'
+);
 
-//localhost
-$sHost = "localhost";
-$sUser = "root";
-$sDb = 'pedicurepraktijksol';
-$password = 'root';
-
-//online!
-// $sHost = "localhost";
-// $sUser = "Nanko";
-// $sDb = 'pedicurepraktijdsol';
-// $password = 'PedicureNanko';
-
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+    // not valid
+    $sHost = "localhost";
+    $sUser = "root";
+    $sDb = 'pedicurepraktijksol';
+    $password = 'root';
+} else {
+    $sHost = "localhost";
+    $sUser = "Nanko";
+    $sDb = 'pedicurepraktijdsol';
+    $password = 'PedicureNanko';
+}
 ?>
