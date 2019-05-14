54737fcdfe469770935261abe86467be81c3a46e
NankoPrinzhorn <memaybeme@hotmail.com>
2019-05-14 09:33:57 +0200

    mac test

--- js/admin/adminSidebar.js
+++ js/admin/adminSidebar.js
@@ -1,8 +1,12 @@
+var disable = false;
 $(".page").on("click", function() {
-    toggleSideBar(this);
+    if (!disable) {
+        toggleSideBar(this);
+    }
 });
 
 function toggleSideBar(el) {
+    disable = true;
     var className = $(el).attr("id");
 
     $('.panel').each(function(){ 
@@ -19,34 +23,44 @@ function toggleSideBar(el) {
 
 
     });
+
+    setTimeout(disable = false, 1000);
 }
 
 function makeActive(current, className) {
+    $('.fake_body.'+className).css('z-index', '5')
     $('#'+className).addClass('active');
     $('.fake_body.'+className).addClass('active_panel full');
+    setTimeout(function() {
+        $('.fake_body.'+className).css('z-index', '3');
+    }, 1000)
 }
 
 function removeActive(current, className) {
-    //listing
-    if ($('#'+className).hasClass('active')) {
-        $('#'+className).removeClass('active');
-        $('#'+className).find('i').css('transform', 'rotate(0deg)');
-    }
-    
-    //fake body
-    if ($('.fake_body.'+className).hasClass('active_panel') || $('.fake_body.'+className).hasClass('full')) {
-        $('.fake_body.'+className).removeClass('active_panel');
-        $('.fake_body.'+className).removeClass('full');
-    }
-
-    //
+    $('.fake_body.'+className).css('z-index', '3');
+    setTimeout(function() {
+        //listing
+        if ($('#'+className).hasClass('active')) {
+            $('#'+className).removeClass('active');
+            $('#'+className).find('i').css('transform', 'rotate(0deg)');
+        }
+        
+        //fake body
+        if ($('.fake_body.'+className).hasClass('active_panel') || $('.fake_body.'+className).hasClass('full')) {
+            $('.fake_body.'+className).removeClass('active_panel');
+            $('.fake_body.'+className).removeClass('full');
+        }
+        $('.panel.'+className).removeClass('active_panel');
+    }, 500);
 }
 
 function toggleEdit(current, className) {
     if ($('.panel.'+className).hasClass('active_panel')) {
         $('.panel.'+className).removeClass('active_panel');
+        $('#'+className).find('i').css('transform', 'rotate(0deg)');
     } else {
         $('.panel.'+className).addClass('active_panel');
+        $('#'+className).find('i').css('transform', 'rotate(45deg)');
     }
     return;
     if ($('.fake_body.'+className).hasClass('full')) {
