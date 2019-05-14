d53fbcfd57dacdccd19c3f927fbc6922a5d95adc
Ysabella <20499@ma-web.nl>
2019-05-14 14:47:32 +0200

    ysa changes

---
+++ views/site/footer.php
@@ -0,0 +1,47 @@
+<link rel="stylesheet" type="text/css" href="/css/site/footer.css">
+
+<html>
+<body>
+
+<!-- footer  -->
+<footer class="footer">
+<div class="container">
+    <div class="row">
+      <div class="col-md-12">
+
+      <div class="col-md-4">
+          <p><strong>Wilt u een afspraak maken of heeft u een vraag?</strong></p>
+          <p>Pedicurepraktijk sol,</p>
+          <p>Telefoon: 06 51304651</p>
+          <p>Mail: pedicurepraktijksol@gmail.com</p>
+          <p>Voor een behandeling in Castricum  kunt u ook bij de medewerkers van <a href="https://cosmohairstyling.com/salons/cosmo-hairstyling-castricum" target="_blank">Cosmo Hairstyling</a> een afspraak maken.</p>
+          <p>KVK :74382713</p>
+          <p>Bank: NL55 ASNB 0781 2720 25</p>
+      </div>
+
+      <div class="col-md-4">
+        <div class="ruimte_boven1"></div>
+          <p><strong>Praktijk aan huis</strong></p>
+          <p>Bergerweg 57, 1816 BN Alkmaar</p>
+          <p>De praktijkruimte bevindt zich op de 1e etage.
+             Er is vrij parkeren aan de Bergerweg en in het Rembrandtkwartier achter de Bergerweg.</p>
+             <iframe class="googlemaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2421.2016134793375!2d4.729247215361559!3d52.63827043521052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf57bd042af8ad%3A0x7118242ce1a7bec5!2sBergerweg+57%2C+1816+BN+Alkmaar!5e0!3m2!1snl!2snl!4v1557823999388!5m2!1snl!2snl"
+             width="360" height="230" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <div class="col-md-4">
+        <div class="ruimte_boven2"></div>
+          <p><strong>Aangesloten bij:</strong></p>
+          <img class="provoet_logo" src="images/logo-provoet.svg" alt="ProVoet logo";>
+          <img class="honk_logo" src="images/logo-honk.svg" alt="Honk logo";>
+      </div>
+
+        </div>
+
+    </div>
+</div>
+</footer>
+<!-- end footer -->
+
+</body>
+</html>
