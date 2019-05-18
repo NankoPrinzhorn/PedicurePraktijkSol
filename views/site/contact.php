b0a2305270e774522399bcfd6f70061794c80f79
maurobertozzi <maurobertozzi11@gmail.com>
2019-05-18 22:54:45 +0200

    contact page

--- views/site/contact.php
+++ views/site/contact.php
@@ -1,1 +1,45 @@
-CONTACT
+<link rel="stylesheet" type="text/css" href="/css/site/contact.css">
+
+<section class="contact-hero">
+  <div class="container">
+    <div class="row">
+      <div class="intro col-md-12">
+      	<!-- <small>Over mij &mdash;</small> -->
+        <h1>Waar vindt u Pedicurepraktijk sol?</h1>
+
+        <div class="header-buttons col-md-12">
+            <button>Bekijk de locatie</button>
+            <button>Neem direct contact op</button>
+        </div>
+      </div>
+  </div>
+</section>
+
+<section class="contact-info">
+	<div class="container">
+		<div class="alkmaar col-md-5">
+            <h4>Alkmaar: praktijk aan huis</h4>
+            <p class="adres-alkmaar"><b>Bergerweg 57, 1816 BN</b></p>
+            <p class="info-alkmaar">De praktijkruimte bevindt zich op de 1e etage. Er is vrij parkeren aan de Bergerweg en in het Rembrandtkwartier achter de Bergerweg.</p>
+            <a href=""><u>Kijk op de kaart</u></a>	
+		</div>
+		<div class="castricum col-md-5 col-md-offset-2">
+            <h4>Castricum</h4> 
+            <p class="info-castricum">op <b>dinsdag</b> houdt Pedicurepraktijk Sol praktijk in sfeervolle salon van Cosmo Hairstyling aan het <b>Bakkerspleintje 24</b> in Castricum. U kunt gedurende 2 uur gratis parkeren met een parkeerschijf in de nabije omgeving.</p> 
+            <a href=""><u>Kijk op de kaart</u></a>	
+        </div>
+	</div>
+</section>
+
+
+<section class="contact-information">
+	<div class="contact-informationInline">
+		<div class="container">
+			<div class="col-md-12">
+                <div class="col-md-6">
+                    <h2>Wilt u een afspraak maken of heeft u een vraag?</h2>
+                </div>       
+			</div>
+		</div>
+	</div>
+</section>
