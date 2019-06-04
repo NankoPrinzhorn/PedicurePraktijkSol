012aeb6b30d3591743f78ce965ca425512b62dae
mauro <maurobertozzi11@gmail.com>
2019-06-04 14:40:28 +0200

    Update contact.php

--- views/site/contact.php
+++ views/site/contact.php
@@ -8,8 +8,8 @@
         <h1>Waar vindt u Pedicurepraktijk sol?</h1>
 
         <div class="header-buttons col-md-12">
-            <button>Bekijk de locatie</button>
-            <button>Neem direct contact op</button>
+            <a href="https://tinyurl.com/y6fh4xwv"><button>Bekijk de locatie</button></a>
+            <a href="mailto:gebruikersnaam@emailadres.nl"><button>Neem direct contact op</button></a>
         </div>
       </div>
   </div>
@@ -24,7 +24,7 @@
             <h4>Alkmaar: praktijk aan huis</h4>
             <p class="adres-alkmaar"><b>Bergerweg 57, 1816 BN</b></p>
             <p class="info-alkmaar">De praktijkruimte bevindt zich op de 1e etage. Er is vrij parkeren aan de Bergerweg en in het Rembrandtkwartier achter de Bergerweg.</p>
-            <a href=""><u>Kijk op de kaart</u></a>
+            <a href="https://tinyurl.com/y6fh4xwv"><u>Kijk op de kaart</u></a>
             </div>	
 		</div>
 
@@ -34,7 +34,7 @@
            
             <h4>Castricum</h4> 
             <p class="info-castricum">op <b>dinsdag</b> houdt Pedicurepraktijk Sol praktijk in sfeervolle salon van Cosmo Hairstyling aan het <b>Bakkerspleintje 24</b> in Castricum. U kunt gedurende 2 uur gratis parkeren met een parkeerschijf in de nabije omgeving.</p> 
-            <a href=""><u>Kijk op de kaart</u></a>	
+            <a href="https://tinyurl.com/y3g5urow"><u>Kijk op de kaart</u></a>	
              </div>
         </div>
 	</div>
@@ -48,8 +48,8 @@
                 <div class="col-md-6">
                     <h2>Wilt u een afspraak maken of heeft u een vraag?</h2>
                     <p>Pedicurepraktijk Sol,</p>
-                    <p><i class="fas fa-phone"></i>06 51304651</p>
-                    <p><i class="fas fa-envelope"></i>pedicurepraktijksol@gmail.nl</p>
+                    <a href="href="tel:+6494461709"><p><i class="fas fa-phone"></i>06 51304651</p></a>
+                    <a href="mailto:gebruikersnaam@emailadres.nl"><p><i class="fas fa-envelope"></i>pedicurepraktijksol@gmail.nl</p></a>
 
                     <div class="reviewInformation">
 					    <h4>Voor een behandeling in Castricum kunt u ook bij de medewerkers van Cosmo Hairstyling een afspraak maken.</h4>
