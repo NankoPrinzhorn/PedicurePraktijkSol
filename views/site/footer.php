1f74ba06e1795b477ce0a3ca14f3879cc881bae1
maurobertozzi <maurobertozzi11@gmail.com>
2019-06-05 11:33:24 +0200

    Update footer.php

--- views/site/footer.php
+++ views/site/footer.php
@@ -25,7 +25,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/site/footer.php";
       <div class="col-md-4">
         <div class="ruimte_boven1"></div>
           <p><strong>Praktijk aan huis</strong></p>
-          <p class="<?=$data[2]['editID']?>" style='white-space: pre-line'><?=$data[2]['text']?></p>
+          <p class="<?=$footerdata[2]['editID']?>" style='white-space: pre-line'><?=$footerdata[2]['text']?></p>
              <iframe class="googlemaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2421.2016134793375!2d4.729247215361559!3d52.63827043521052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf57bd042af8ad%3A0x7118242ce1a7bec5!2sBergerweg+57%2C+1816+BN+Alkmaar!5e0!3m2!1snl!2snl!4v1557823999388!5m2!1snl!2snl"
              width="100%"  height="275px" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
