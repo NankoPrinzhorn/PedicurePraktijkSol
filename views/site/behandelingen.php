ad88b19d43803733b16212b6c690610a08f41ce9
maurobertozzi <maurobertozzi11@gmail.com>
2019-06-05 21:04:00 +0200

    pdf's werken nu HYPE

--- views/site/behandelingen.php
+++ views/site/behandelingen.php
@@ -84,11 +84,15 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/site/behandelingen.php";
 
 						<?php 
 						foreach ($technieken as $techniek) {
+							$pdfFile = str_replace(' ', '%20', $techniek[5]['text']);
+							// var_dump($pdfFile);
+							// die();
+
 							echo '
 							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 								<h4 class="'.$techniek[2]['editID'].'">'.$techniek[2]['text'].'</h4>
 								<p class="'.$techniek[3]['editID'].'">'.$techniek[3]['text'].'</p>
-								<a class="behandeling--left-link" href="#">Bekijk het informatieblad over deze
+								<a class="behandeling--left-link '.$techniek[5]['editID'].'" href="/images/uploads/'.$pdfFile.'.pdf">Bekijk het informatieblad over deze
 									behandeling</a>
 								<div class="behandeling--case--img '.$techniek[4]['editID'].'" style="background-image: url(/images/uploads/'.$techniek[4]['text'].');">
 								</div>
