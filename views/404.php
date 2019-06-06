2ec0679e7ce364de2f2598206b2c0c821b47550a
Ravi Stil <ravi.raaf@gmail.com>
2019-06-06 13:23:42 +0200

    test

--- views/404.php
+++ views/404.php
@@ -1,1 +1,9 @@
-404!
+<link rel="stylesheet" type="text/css" href="/css/site/404.css">
+
+<section class="section-404 container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Oops</h1>
+        </div>
+    </div>
+</section>
