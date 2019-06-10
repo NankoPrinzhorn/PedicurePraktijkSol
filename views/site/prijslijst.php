19cc3e40500a1d824c491e6c1da9ed3661281798
NankoPrinzhorn <memaybeme@hotmail.com>
2019-06-10 16:31:56 +0200

    prijslijst fix

--- views/site/prijslijst.php
+++ views/site/prijslijst.php
@@ -62,7 +62,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/site/prijslijst.php";
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2" align="center">
 					<h2 class="<?=$data[4]['editID']?>"><?=$data[4]['text']?></h2>
-					<p class="<?=$data[5]['editID']?>" style="white-space: pre-line"><?=$data[5]['text']?></p>
+					<p class="<?=$data[5]['editID']?>" style="white-space: pre-line"><?=str_replace("www.zorgwijzer.nl/vergoedingen/pedicure", "<a href='www.zorgwijzer.nl/vergoedingen/pedicure'>zorgwijzer.nl</a>", $data[5]['text'])?></p>
 				</div>
 			</div>
 		</div>
